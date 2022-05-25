728eea382a093237a912c7a04ddc2670b463414d
melaniebalam <melanie.pucbalam@cbtis72.edu.mx>
2022-05-25 11:50:36 -0500

    Subiendo la carpeta 64

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@
   var regalo = document.getElementById('regalo');
   document.addEventListener('DOMContentLoaded', function () {
 
-    var map = L.map('mapa').setView([19.575965, -448.03791], 17);
+    //var map = L.map('mapa').setView([19.575965, -448.03791], 17);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    //L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //}).addTo(map);
 
-    L.marker([19.575965, -448.03791]).addTo(map)
-      .bindPopup('GDLWebCamp <br> Boletos ya Disponibles')
-      .openPopup();
+    //L.marker([19.575965, -448.03791]).addTo(map)
+    //  .bindPopup('GDLWebCamp <br> Boletos ya Disponibles')
+    //  .openPopup();
 
     //Campos Datos de Uusuario
     var nombre = document.getElementById('nombre');
