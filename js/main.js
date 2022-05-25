b796d2b795c14f656435d0c795c00b7e6d8eb0f3
marianalG0 <garciahaumariana@gmail.com>
2022-05-25 08:19:06 -0500

    Subiendo la parte de la carpeta 61

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@
   var regalo = document.getElementById('regalo');
   document.addEventListener('DOMContentLoaded', function () {
 
+    var map = L.map('mapa').setView([19.575965, -448.03791], 17);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([19.575965, -448.03791]).addTo(map)
+      .bindPopup('GDLWebCamp <br> Boletos ya Disponibles')
+      .openPopup();
+
     //Campos Datos de Uusuario
     var nombre = document.getElementById('nombre');
     var apellido = document.getElementById('apellido');
@@ -35,25 +45,25 @@
     email.addEventListener('blur', validarCampos);
     email.addEventListener('blur', validarMail);
 
-    function validarCampos(){
-      if(this.value == ''){
+    function validarCampos() {
+      if (this.value == '') {
         errorDiv.style.display = 'block';
-        errorDiv.innerHTML= "Este campo es obligatorio";
+        errorDiv.innerHTML = "Este campo es obligatorio";
         this.style.border = '1px solid red';
         errorDiv.style.border = '1px solid red';
-      }else{
+      } else {
         errorDiv.style.display = 'none';
         this.style.border = '1px solid #cccccc';
       }
     }
 
-    function validarMail(){
-      if(this.value.indexOf("@")> -1){
+    function validarMail() {
+      if (this.value.indexOf("@") > -1) {
         errorDiv.style.display = 'none';
         this.style.border = '1px solid #cccccc';
-      }else{
+      } else {
         errorDiv.style.display = 'block';
-        errorDiv.innerHTML= "Este campo debe tener al menos un @";
+        errorDiv.innerHTML = "Este campo debe tener al menos un @";
         this.style.border = '1px solid red';
         errorDiv.style.border = '1px solid red';
       }
