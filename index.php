8c2a40636ab24cccc08c5baa6e21d10aeec567b7
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2022-05-25 15:17:55 -0400

    Se añade el slider
    Se hacen los cambios correspondientes para el slider

--- index.php
+++ index.php
@@ -101,11 +101,36 @@
 
   <section class="programa">
     <div class="contenedor-video">
-      <video autoplay loop poster="bg-talleres.jpg">
-        <source src="video/video.mp4" type="video/mp4" />
-        <source src="video/video.webm" type="video/webm" />
-        <source src="video/video.ogv" type="video/ogg" />
-      </video>
+    <div class="slider">
+          <?php
+              $ids = array(1,2,3,4,5);
+              $alt = array(
+                  "Slide 1",
+                  "Slide 2",
+                  "Slide 3",
+                  "Slide 4",
+                  "Slide 5"
+              );
+              $max = count($ids);
+              for($s=0;$s<$max;$s++){ ?>
+                  <input type="radio" id="<?= $ids[$s]; ?>" name="image-slide" hidden />
+              <?php }
+          ?>
+          <div class="slideshow">
+              <?php for($s=0;$s<$max;$s++){ ?>
+              <div class="item-slide">
+                  <img src="img/<?= $ids[$s]; ?>.jpg" alt="<?= $alt[$s]; ?>" />
+              </div>
+              <?php } ?>
+          </div>
+          <div class="pagination">
+              <?php for($s=0;$s<$max;$s++){ ?>
+              <label class="pag-item" for="<?= $ids[$s]; ?>">
+                  <img src="img/<?= $ids[$s]; ?>.jpg" alt="<?= $alt[$s]; ?>" />
+              </label>
+              <?php } ?>
+          </div>
+      </div>
     </div>
     <!--.contenedor-video-->
 
